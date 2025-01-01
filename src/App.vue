c53a57b4386c86a37b91fb89b665eb0f43b3603b
allyelvis <allyelvis>
2025-01-01 19:12:05 +0000

    build package

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ProductList from './components/ProductList.vue'
+import AddProduct from './components/AddProduct.vue'
+import POS from './components/POS.vue'
+import SaleTransaction from './components/SaleTransaction.vue'
+</script>
+
+<template>
+  <div>
+    <h1>POS and Inventory System</h1>
+    <ProductList />
+    <AddProduct />
+    <POS />
+    <SaleTransaction />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b883;
+  text-align: center;
+}
+</style>
